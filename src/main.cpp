c28f1c091f44a81679949c8096a474a14751cf16
Bader Alshaya <alshayabader@gmail.com>
2018-01-14 04:33:27 -0800

    V2.2: Overloaded the following operators for the BST class: =, +=, <<

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main()
         cout << "ERROR! THE FILE IS EMPTY!\n";
     else
     {
-        BST obj1, obj2, obj3; //one tree to store all the data, another to retrieve some data
+        BST obj1, obj2, obj3;        //one tree to store all the data, another to retrieve some data
         string key = "Isaac Newton"; //a key string to use for "display/remove/retrieve matches" functions
 
         while (!file.eof()) //insert the data of the file to the BST
@@ -26,7 +26,7 @@ int main()
         cout << "---------------------------------------------------------------------\n";
         cout << "TEST BST FUNCTIONS\n";
         cout << "---------------------------------------------------------------------\n";
-        
+
         cout << "ORIGINAL LIST:\n"; //display the content of the entire BST object after inserting
         cout << obj1 << " item(s) in the tree\n";
         cout << "---------------------------------------------------------------------\n";
@@ -37,19 +37,19 @@ int main()
         obj1.retrieve(obj2, key); //retrieves all instances of a key string
         cout << obj2 << " item(s) retrieved!\n";
         cout << "---------------------------------------------------------------------\n";
-        
+
         cout << "Looking for matches to DISPLAY:\n"; //display all instances of a key string
         cout << obj1.display(key) << " matche(s) displayed!\n";
         cout << "---------------------------------------------------------------------\n";
-        
+
         cout << "Looking for matches to REMOVE:\n"; //remove all instances of a key string
         cout << obj1.remove(key) << " matche(s) removed!\n";
         cout << "---------------------------------------------------------------------\n";
-        
+
         cout << "MODIFIED LIST:\n"; //display the content of the entire BST object after removing
         cout << obj1 << " item(s) in the tree\n";
         cout << "---------------------------------------------------------------------\n";
-        
+
         cout << "COPIED (ORIGINAL) LIST:\n"; //display the list that has been copied using the = operator
         cout << obj3 << " item(s) in the tree\n";
         cout << "---------------------------------------------------------------------\n";
